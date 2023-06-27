7eb16b3d14b7f4096a9e6b3256bff68e8ebbe118
BlakeJC94 <blakejamescook@gmail.com>
2023-06-28 08:49:42 +1000

    dev: entrypoint stubs

--- torch_lab/__init__.py
+++ torch_lab/__init__.py
@@ -2,16 +2,20 @@ from importlib import metadata
 
 from .core import Config, LabModule, LabDataModule
 from .log import setup_logging
-from .train import train
 from .build import build
+from .train import train
+from .test import test
+from .predict import predict
 
 
 setup_logging()
 
 __all__ = [
     "__version__",
-    "train",
     "build",
+    "train",
+    "test",
+    "predict",
 ]
 
 __version__ = metadata.version("torch_lab")
