4442ee67a222c885fcff346addb4d08917a7833c
BlakeJC94 <blakejamescook@gmail.com>
2023-06-27 20:13:43 +1000

    feat: fire entrypoint

--- torch_lab/__main__.py
+++ torch_lab/__main__.py
@@ -1,5 +1,17 @@
+import fire
+
+from . import train, build, __version__
+
+
 def main():
-    ...
+    fire.Fire(
+        dict(
+            version=str(__version__),
+            train=train,
+            build=build,
+        )
+    )
+
 
 if __name__ == "__main__":
     main()
