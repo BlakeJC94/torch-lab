9e6b5328719e8b9b3cf5325add5df217ac3a2851
BlakeJC94 <blakejamescook@gmail.com>
2024-02-22 09:01:25 +1100

    Added dataset class

---
+++ hms_brain_activity/dataset.py
@@ -0,0 +1,71 @@
+import logging
+from pathlib import Path
+from typing import Tuple, Optional, Callable
+
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+from .globals import CHANNEL_NAMES, VOTE_NAMES
+
+
+logger = logging.getLogger(__name__)
+
+
+class HmsLocalDataset(Dataset):
+    """Dataset class for loading time series data"""
+
+    sample_rate = 200.0  # Hz
+    sample_secs = 50.0  # s
+    channel_names = CHANNEL_NAMES
+    vote_names = VOTE_NAMES
+
+    def __init__(
+        self,
+        data_dir: str | Path,
+        annotations: pd.DataFrame,
+        transform: Optional[Callable] = None,
+    ):
+        self.data_dir = Path(data_dir).expanduser()
+        self.annotations = annotations
+        self.transform = transform
+
+        assert self.data_dir.exists()
+
+        self.filepaths = list(self.data_dir.glob("*.parquet"))
+
+        n_files = len(self.filepaths)
+        n_eeg_ids = self.annotations["eeg_id"].nunique()
+        if n_files < n_eeg_ids:
+            logger.warning(
+                f"Only {n_files} / {n_eeg_ids} were found, filtering annotations list"
+            )
+            found_eeg_ids = set(int(fp.stem) for fp in self.filepaths)
+            self.annotations = self.annotations[
+                self.annotations["eeg_id"].isin(found_eeg_ids)
+            ]
+
+        if len(self.annotations) == 0:
+            raise ValueError("No samples found.")
+
+    def __len__(self) -> int:
+        return len(self.annotations)
+
+    def __getitem__(self, idx: int) -> Tuple[np.ndarray, np.ndarray]:
+        md = self.annotations.iloc[idx]
+        start = int(md["eeg_label_offset_seconds"] * self.sample_rate)
+        duration = int(self.sample_rate * self.sample_secs)
+
+        eeg_id = md["eeg_id"]
+        eeg_path = self.data_dir / f"{eeg_id}.parquet"
+        data = pd.read_parquet(eeg_path)
+        data = data[self.channel_names]
+        data = data.iloc[start : start + duration].to_numpy()
+
+        label = md[self.vote_names].to_numpy()
+
+        if self.transform:
+            data, label = self.transform(data, label)
+
+        return data, label
