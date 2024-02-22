46f7eee408b0ef182b7b9ae9c83c406da399dda9
BlakeJC94 <blakejamescook@gmail.com>
2024-02-22 14:29:19 +1100

    Add basic config to check all is working

---
+++ experiments/00_initial/__init__.py
@@ -0,0 +1,108 @@
+import os
+from pathlib import Path
+from functools import partial
+
+import pytorch_lightning as pl
+import pandas as pd
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchmetrics.classification import MulticlassAccuracy
+from torchvision.transforms.v2 import Compose
+
+from hms_brain_activity.metadata_classes import ModelConfig
+from hms_brain_activity.module import MainModule
+from hms_brain_activity.datasets import HmsLocalClassificationDataset
+from hms_brain_activity import transforms as t
+
+
+class PlaceholderModel(nn.Module):
+    def __init__(self, num_channels, num_classes):
+        super().__init__()
+        self.conv = nn.Conv1d(num_channels, num_classes, kernel_size=1)
+        self.mean = nn.AvgPool1d()
+
+    def forward(self, x):
+        x = self.conv(x)
+        return self.avg(x)
+
+
+def config(hparams) -> ModelConfig:
+    num_channels = 20
+    num_classes = 6
+
+    module = MainModule(
+        PlaceholderModel(num_channels=num_channels, num_classes=num_classes),
+        loss_function=nn.MSELoss(),
+        metrics={
+            "accuracy": MulticlassAccuracy(num_classes=num_classes),
+        },
+        optimizer_factory=partial(
+            optim.AdamW,
+            lr=hparams["config"]["learning_rate"],
+            weight_decay=hparams["config"]["weight_decay"],
+        ),
+        scheduler_factory=lambda opt: {
+            "scheduler": optim.lr_scheduler.MultiStepLR(
+                opt,
+                milestones=[20, 40, 60],
+                gamma=0.2,
+            ),
+            "monitor": hparams["config"]["monitor"],
+        },
+    )
+
+    annotations = pd.read_csv("./data/hms/train.csv")
+
+    # TODO Find a better subsampling strategy
+    val_patient_ids = set(annotations['patient_id'].unique().sample(frac=0.2, random_seed=0))
+    val_annotations = annotations[annotations['patient_ids'].isin(val_patient_ids)]
+    train_annotations = annotations[~annotations['patient_ids'].isin(val_patient_ids)]
+
+    data_dir = "./data/hms/train_eegs"
+
+    train_dataset = HmsLocalClassificationDataset(
+        data_dir=data_dir,
+        annotations=train_annotations,
+        transform=Compose(
+            [
+                t.ToTensor(),
+            ]
+        ),
+    )
+
+    val_dataset = HmsLocalClassificationDataset(
+        data_dir=data_dir,
+        annotations=val_annotations,
+        transform=Compose(
+            [
+                t.ToTensor(),
+            ]
+        ),
+    )
+
+    return ModelConfig(
+        project=hparams["task"]["init"]["project_name"],
+        experiment_name="-".join(Path(__file__).parts[-2:]),
+        model=module,
+        train_dataloader=DataLoader(
+            train_dataset,
+            batch_size=hparams["config"]["batch_size"],
+            num_workers=hparams["config"].get("num_workers") or (os.cpu_count() or 0),
+            shuffle=True,
+        ),
+        val_dataloader=DataLoader(
+            val_dataset,
+            batch_size=hparams["config"]["batch_size"],
+            num_workers=hparams["config"].get("num_workers") or (os.cpu_count() or 0),
+            shuffle=False,
+        ),
+        callbacks=[
+            pl.callbacks.EarlyStopping(
+                monitor="loss/val",
+                min_delta=0.0001,
+                patience=hparams["config"]["patience"],
+                verbose=True,
+                mode="min",
+            ),
+        ],
+    )
---
+++ experiments/00_initial/baseline.py
@@ -0,0 +1,44 @@
+hparams = {
+    "task": {
+        "init": {
+            "project_name": "HMS",
+        },
+        # "parent_task_id": "2e6cb27f9db54a28bf841a3418794629",
+    },
+    "checkpoint": {
+        "checkpoint_task_id": None,
+        # "checkpoint_name": "last",
+        # "weights_only": False,
+    },
+    "trainer": {
+        "init": {
+            "enable_progress_bar": False,
+        },
+    },
+    "config": {
+        "learning_rate": 3 * 1e-4,
+        "weight_decay": 0.01,
+        "batch_size": 8,
+        "patience": 20,
+        "monitor": "loss/val",
+    },
+}
+
+if __name__ == "__main__":
+    from pathlib import Path
+    exec((Path(__file__).parent / "__init__.py").open().read())
+    conf = config(hparams)
+    model = conf.model
+
+    import torch
+    n_timesteps = 50 * 200
+    x = torch.rand(hparams["config"]["batch_size"], 20, n_timesteps)
+    y = torch.rand(hparams["config"]["batch_size"], 6)
+
+    out = model(x)
+    try:
+        loss = model.loss_function(out, y)
+    except:
+        breakpoint()
+
+    print("Pass!")
