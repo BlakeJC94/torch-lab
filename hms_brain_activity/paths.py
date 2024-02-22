40b2bab591e9160e1eec2b5db1ae24b4c5aaca99
BlakeJC94 <blakejamescook@gmail.com>
2024-02-22 11:04:37 +1100

    Add task dir getter

--- hms_brain_activity/paths.py
+++ hms_brain_activity/paths.py
@@ -5,3 +5,6 @@ ROOT_DIR = PROJECT_DIR.parent
 
 ARTIFACTS_DIR = ROOT_DIR / "artifacts"
 ARTIFACTS_DIR.mkdir(exists_ok=True)
+
+def get_task_artifacts_dir(task) -> Path:
+    return ARTIFACTS_DIR / f"{task.name}-{task.id}"
