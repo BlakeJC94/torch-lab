4feccf33e926c746be4ef8ca56a35ad6eff59f21
BlakeJC94 <blakejamescook@gmail.com>
2024-02-23 09:46:05 +1100

    Add Montage

--- hms_brain_activity/transforms.py
+++ hms_brain_activity/transforms.py
@@ -1,5 +1,9 @@
+from typing import List, Tuple
+
 import torch
 from torch import nn
+
+from hms_brain_activity.globals import CHANNEL_NAMES
 
 
 class ToTensor(nn.Module):
@@ -35,3 +39,53 @@ class ScaleEEG(nn.Module):
     def forward(self, x, md):
         x[:-1, ...] = x[:-1, ...] / self.scalar
         return x, md
+
+
+class _BaseMontage(nn.Module):
+    montage: List[Tuple[str, str]]
+
+    def __init__(self):
+        super().__init__()
+        n_channels = len(CHANNEL_NAMES)
+        montage_mat = torch.zeros((n_channels, n_channels))
+        for j, (ch_1, ch_2) in enumerate(self.montage):
+            ch_idx_1 = CHANNEL_NAMES.index(ch_1) if ch_1 in CHANNEL_NAMES else None
+            if ch_idx_1 is not None:
+                montage_mat[ch_idx_1, j] = 1
+
+            ch_idx_2 = CHANNEL_NAMES.index(ch_2) if ch_2 in CHANNEL_NAMES else None
+            if ch_idx_2 is not None:
+                montage_mat[ch_idx_2, j] = -1
+
+        self.montage_mat = montage_mat
+
+    def forward(self, x, md=None):
+        x = torch.matmul(
+            x.transpose(-2, -1),
+            self.montage_mat,
+        ).transpose(-2, -1)
+        return x, md
+
+
+class DoubleBananaMontage(_BaseMontage):
+    montage = [
+        ("Fp1", "F7"),
+        ("F7", "T3"),
+        ("T3", "T5"),
+        ("T5", "O1"),
+        ("Fp2", "F8"),
+        ("F8", "T4"),
+        ("T4", "T6"),
+        ("T6", "O2"),
+        ("Fp1", "F3"),
+        ("F3", "C3"),
+        ("C3", "P3"),
+        ("P3", "O1"),
+        ("Fp2", "F4"),
+        ("F4", "C4"),
+        ("C4", "P4"),
+        ("P4", "O2"),
+        ("Fz", "Cz"),
+        ("Cz", "Pz"),
+        ("EKG", ""),
+    ]
---
+++ tests/test_transforms.py
@@ -0,0 +1,33 @@
+import torch
+import pytest
+
+from hms_brain_activity import transforms as t
+from hms_brain_activity.globals import CHANNEL_NAMES
+
+
+class MockMontage(t._BaseMontage):
+    montage = [("Fp1", "F3"), ("P3", "C3"), ("F7", "")]  # 0, 1  # 3, 2  # 4
+
+
+@pytest.mark.parametrize(
+    "shape",
+    [
+        (len(CHANNEL_NAMES), 30),  # Sample
+        (4, len(CHANNEL_NAMES), 30),  # Batch
+    ],
+)
+def test_base_montage(shape):
+    """Test that the _BaseMontage.forward method works as expected."""
+    ## Assemble
+    transform = MockMontage()
+    data = torch.rand(len(CHANNEL_NAMES), 30)  # channels, timesteps
+    metadata = dict(foo="bar")
+
+    ## Act
+    output, md = transform(data, metadata)
+
+    ## Assert
+    assert md == metadata
+    assert (output[0, :] == data[0, :] - data[1, :]).all()
+    assert (output[1, :] == data[3, :] - data[2, :]).all()
+    assert (output[2, :] == data[4, :]).all()
