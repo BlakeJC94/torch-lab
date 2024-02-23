13b30077a73b78b52fc43cdf6d600fa6e4fd54bd
BlakeJC94 <blakejamescook@gmail.com>
2024-02-23 18:33:58 +1100

    Rfc transform into base class

--- hms_brain_activity/transforms.py
+++ hms_brain_activity/transforms.py
@@ -1,3 +1,4 @@
+import abc
 import random
 from typing import List, Tuple
 
@@ -7,49 +8,57 @@ from torch import nn
 from hms_brain_activity.globals import CHANNEL_NAMES
 from hms_brain_activity.utils import saggital_flip_channel
 
+class _BaseTransform(nn.Module, abc.ABC):
+    @abc.abstractmethod
+    def compute(x, md):
+        return x, md
 
-class ToTensor(nn.Module):
+    def forward(self, x, md=None):
+        x, md = self.compute(x, md)
+        if md is None:
+            return x
+        return x, md
+
+
+
+class ToTensor(_BaseTransform):
     def __init__(self, nan_to_num=0):
         super().__init__()
         self.nan_to_num = nan_to_num
 
-    def forward(self, x, md):
+    def compute(self, x, md):
         x = torch.nan_to_num(torch.tensor(x), self.nan_to_num)
         md["y"] = torch.nan_to_num(torch.tensor(md["y"]), self.nan_to_num)
         return x, md
 
 
-class VotesToProbabilities(nn.Module):
-    def forward(self, x, md):
+class VotesToProbabilities(_BaseTransform):
+    def compute(self, x, md):
         y = md["y"]
         y = y / y.sum(axis=0).unsqueeze(0)
         md["y"] = y
         return x, md
 
 
-class TanhClipTensor(nn.Module):
+class TanhClipTensor(_BaseTransform):
     def __init__(self, abs_bound: float):
         super().__init__()
         self.abs_bound = abs_bound
 
-    def forward(self, x, md=None):
+    def compute(self, x, md):
         x = torch.tanh(x / self.abs_bound) * self.abs_bound
-        if md is None:
-            return x
         return x, md
 
 
-class _BaseScaleChannels(nn.Module):
+class _BaseScaleChannels(_BaseTransform, abc.ABC):
     def __init__(self, scalar: float):
         super().__init__()
         self.scalar = scalar
 
-    def forward(self, x, md=None):
+    def compute(self, x, md):
         x_slice = [slice(None)] * x.ndim
         x_slice[-2] = self.ch_slice
         x[x_slice] = x[x_slice] / self.scalar
-        if md is None:
-            return x
         return x, md
 
 
@@ -61,7 +70,7 @@ class ScaleECG(_BaseScaleChannels):
     ch_slice = slice(-1, None)
 
 
-class _BaseMontage(nn.Module):
+class _BaseMontage(_BaseTransform, abc.ABC):
     montage: List[Tuple[str, str]]
 
     def __init__(self):
@@ -79,7 +88,7 @@ class _BaseMontage(nn.Module):
 
         self.register_buffer("montage_mat", montage_mat)
 
-    def forward(self, x, md=None):
+    def compute(self, x, md):
         x = torch.matmul(
             x.transpose(-2, -1),
             self.montage_mat,
@@ -112,22 +121,22 @@ class DoubleBananaMontage(_BaseMontage):
 
 
 class RandomSaggitalFlip(_BaseMontage):
-    montage = [saggital_flip_channel(ch) for ch in CHANNEL_NAMES]
+    montage = [(saggital_flip_channel(ch), "") for ch in CHANNEL_NAMES]
 
-    def forward(self, x, md):
+    def compute(self, x, md):
         if random.random() < 0.5:
-            x, md = super().forward(x, md)
+            x, md = super().compute(x, md)
         return x, md
 
 
-class RandomScale(nn.Module):
+class RandomScale(_BaseTransform):
     def __init__(self, min_scale=0.75, max_scale=1.25, per_channel=True):
         super().__init__()
         self.min_scale = min_scale
         self.max_scale = max_scale
         self.per_channel = per_channel
 
-    def forward(self, x, md=None):
+    def compute(self, x, md):
         size = x.shape[:-1] if self.per_channel else x.shape[:-2]
         scale = self.min_scale + (self.max_scale - self.min_scale) * torch.rand(size)
         x = x * scale.unsqueeze(-1)
--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -21,13 +21,11 @@ def test_base_montage(shape):
     ## Assemble
     transform = MockMontage()
     data = torch.rand(shape)
-    metadata = dict(foo="bar")
 
     ## Act
-    output, md = transform(data, metadata)
+    output = transform(data)
 
     ## Assert
-    assert md == metadata
     assert (output[..., 0, :] == data[..., 0, :] - data[..., 1, :]).all()
     assert (output[..., 1, :] == data[..., 3, :] - data[..., 2, :]).all()
     assert (output[..., 2, :] == data[..., 4, :]).all()
@@ -45,13 +43,11 @@ def test_random_scale(shape):
     ## Assemble
     transform = t.RandomScale()
     data = torch.rand(shape)
-    metadata = dict(foo="bar")
 
     ## Act
-    output, md = transform(data, metadata)
+    output = transform(data)
 
     ## Assert
-    assert md == metadata
     assert output.ndim == data.ndim
     assert (output < transform.max_scale * data).all()
     assert (output > transform.min_scale * data).all()
