da51e8724bdd8aba101aa6b08339e257c7ff3617
BlakeJC94 <blakejamescook@gmail.com>
2024-02-23 14:48:42 +1100

    Corrected test for BaseMontage

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -20,7 +20,7 @@ def test_base_montage(shape):
     """Test that the _BaseMontage.forward method works as expected."""
     ## Assemble
     transform = MockMontage()
-    data = torch.rand(len(CHANNEL_NAMES), 30)  # channels, timesteps
+    data = torch.rand(shape)
     metadata = dict(foo="bar")
 
     ## Act
@@ -28,9 +28,9 @@ def test_base_montage(shape):
 
     ## Assert
     assert md == metadata
-    assert (output[0, :] == data[0, :] - data[1, :]).all()
-    assert (output[1, :] == data[3, :] - data[2, :]).all()
-    assert (output[2, :] == data[4, :]).all()
+    assert (output[..., 0, :] == data[..., 0, :] - data[..., 1, :]).all()
+    assert (output[..., 1, :] == data[..., 3, :] - data[..., 2, :]).all()
+    assert (output[..., 2, :] == data[..., 4, :]).all()
 
 
 @pytest.mark.parametrize(
