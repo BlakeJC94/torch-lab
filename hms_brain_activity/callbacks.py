9a9c7e273ba2effbc3705971f3891cc3858bb12f
BlakeJC94 <blakejamescook@gmail.com>
2024-02-24 10:08:01 +1100

    Add logging of batch sizes

--- hms_brain_activity/callbacks.py
+++ hms_brain_activity/callbacks.py
@@ -1,10 +1,18 @@
+import logging
 import pytorch_lightning as pl
 
+logger = logging.getLogger(__name__)
 
 class EpochProgress(pl.Callback):
     """Dead simple callback to print a message when an epoch completes (a quieter alternative to the
     progress bar).
     """
 
+    def on_train_start(self, trainer, module):
+        logger.info(f"Starting training with {trainer.num_training_batches} batches")
+
+    def on_validation_start(self, trainer, module):
+        logger.info(f"Starting validation with {trainer.num_val_batches} batches")
+
     def on_train_epoch_end(self, trainer, module):
-        print(f"Finished epoch {module.current_epoch + 1:04}")
+        logger.info(f"Finished epoch {module.current_epoch + 1:04}")
