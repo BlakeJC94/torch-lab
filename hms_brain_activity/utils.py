0901b50d6365a3e9c0724a29e9b9f305cb386b01
BlakeJC94 <blakejamescook@gmail.com>
2024-02-23 19:15:14 +1100

    Add guards for import script util

--- hms_brain_activity/utils.py
+++ hms_brain_activity/utils.py
@@ -5,6 +5,11 @@ from typing import Any
 
 
 def import_script_as_module(config_path) -> Any:
+    config_path = Path(config_path)
+    if not config_path.exists():
+        raise FileNotFoundError(f"Couldn't find '{str(config_path)}'.")
+    if not config_path.is_file():
+        raise FileNotFoundError(f"Path '{str(config_path)}' is not a file.")
     name = Path(config_path).stem
     spec = spec_from_file_location(name, config_path)
     module = module_from_spec(spec)
