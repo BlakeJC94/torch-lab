50cba1245806a137c6d59a8810263df2da976fd3
BlakeJC94 <blakejamescook@gmail.com>
2024-02-26 15:23:38 +1100

    Added SubmissionWriter

--- hms_brain_activity/callbacks.py
+++ hms_brain_activity/callbacks.py
@@ -1,7 +1,14 @@
 import logging
 import pytorch_lightning as pl
+from pathlib import Path
+
+import pandas as pd
+
+from hms_brain_activity.globals import VOTE_NAMES
+
 
 logger = logging.getLogger(__name__)
+
 
 class EpochProgress(pl.Callback):
     """Dead simple callback to print a message when an epoch completes (a quieter alternative to the
@@ -16,3 +23,40 @@ class EpochProgress(pl.Callback):
 
     def on_train_epoch_end(self, trainer, module):
         logger.info(f"Finished epoch {module.current_epoch + 1:04}")
+
+
+class SubmissionWriter(pl.callbacks.BasePredictionWriter):
+    filename = "submission.csv"
+
+    def __init__(self, output_dir: Path):
+        super().__init__(write_interval="batch")
+        self.output_dir = Path(output_dir)
+
+        self.output_path = self.output_dir / self.filename
+        if self.output_path.exists():
+            self.output_path.unlink()
+
+    def write_on_batch_end(
+        self,
+        _trainer,
+        _pl_module,
+        prediction,
+        _batch_indices,
+        _batch,
+        _batch_idx,
+        _dataloader_idx=0,
+    ):
+        out, md = prediction["out"], prediction["md"]
+        out = out.cpu().unsqueeze(-1).numpy()
+        eeg_ids = md['eeg_id'].cpu().numpy()
+
+        rows = pd.DataFrame(
+            {
+                "eeg_id": eeg_ids,
+                **{col: out[:,i] for i, col in enumerate(VOTE_NAMES)}
+            }
+        )
+        rows = rows[["eeg_id", *VOTE_NAMES]]
+
+        mode = "w" if not self.output_path.exists() else "a"
+        rows.to_csv(self.output_path, index=False, mode=mode)
