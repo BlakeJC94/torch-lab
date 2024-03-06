70c3ea7333f7b3d32b8cc89b3ddd9b03617e656e
BlakeJC94 <blakejamescook@gmail.com>
2024-03-06 21:27:07 +1100

    Re-write train

--- tasks/train.py
+++ tasks/train.py
@@ -6,11 +6,12 @@ from typing import Any, Dict, Tuple
 import pytorch_lightning as pl
 import torch
 from clearml import Task
-from pytorch_lightning.loggers import TensorBoardLogger
 
 from hms_brain_activity import logger
+from hms_brain_activity.paths import ARTIFACTS_DIR
+from hms_brain_activity.loggers import ClearMlLogger
 from hms_brain_activity.callbacks import EpochProgress
-from hms_brain_activity.paths import get_task_artifacts_dir
+from hms_brain_activity.paths import get_task_dir_name
 from hms_brain_activity.utils import import_script_as_module, print_dict
 
 
@@ -31,11 +32,11 @@ def train(
     hparams_path: str,
     dev_run: bool = False,
     pdb: bool = False,
-    offline: bool=False,
+    offline: bool = False,
 ) -> str:
     """Choo choo"""
     pl.seed_everything(0, workers=True)
-    torch.set_float32_matmul_precision('high')
+    torch.set_float32_matmul_precision("high")
 
     if dev_run:
         logger.info("DEV RUN")
@@ -44,11 +45,31 @@ def train(
     if offline:
         logger.info("PDB")
 
-    hparams, task, config = setup_task(hparams_path, dev_run, pdb, offline)
-    save_dir = get_task_artifacts_dir(task) / "train"
-    weights_dir = save_dir / "model_weights"
-    for fp in weights_dir.glob("*.ckpt"):
-        fp.unlink()
+    # Get config from hparams
+    hparams = get_hparams(hparams_path, dev_run)
+    config_path = hparams["config"].get(
+        "path",
+        str(Path(hparams_path).parent / "__init__.py"),
+    )
+    task_name = "-".join(Path(hparams_path).parts[-2:]).removesuffix(".py")
+    if dev_run:
+        task_name = f"DEV RUN: {task_name}"
+
+    # Initialise logger
+    clearml_logger = ClearMlLogger(
+        hparams=hparams,
+        config_path=config_path,
+        task_name=task_name,
+        root_dir=ARTIFACTS_DIR,
+        dev_run=dev_run,
+        offline=offline,
+    )
+    task = clearml_logger.task
+    save_dir = ARTIFACTS_DIR / f"{get_task_dir_name(task)}/train"
+
+    # Compile config (and get previous checkpoint if requested)
+    config = compile_config(hparams, config_path, pdb)
+    hparams, config = load_weights(hparams, config)
 
     # Initialise callbacks
     callbacks = [
@@ -59,7 +80,7 @@ def train(
     if not dev_run:
         callbacks = [
             pl.callbacks.ModelCheckpoint(
-                weights_dir,
+                save_dir / "model_weights",
                 monitor=hparams["config"]["monitor"],
                 save_last=True,
             ),
@@ -70,45 +91,13 @@ def train(
     trainer_init_kwargs = hparams["trainer"].get("init", {})
     trainer_fit_kwargs = hparams["trainer"].get("fit", {})
     trainer_init_kwargs = {
-        "logger": TensorBoardLogger(
-            save_dir=str(save_dir / "logs"),
-            name="",
-            default_hp_metric=False,
-        ),
+        "logger": clearml_logger,
         "accelerator": "gpu" if torch.cuda.is_available() else "cpu",
         "callbacks": callbacks,
         "num_sanity_val_steps": 0,
         "max_epochs": -1,
         **trainer_init_kwargs,
     }
-
-    # Download checkpoint
-    ckpt_params = hparams["checkpoint"]
-    checkpoint_task_id = ckpt_params.get("checkpoint_task_id")
-    checkpoint_name = ckpt_params.get("checkpoint_name", "last")
-    weights_only = bool(ckpt_params.get("weights_only", False))
-    if checkpoint_task_id:
-        prev_task = Task.get_task(task_id=checkpoint_task_id)
-        temp_path = (
-            get_task_artifacts_dir(prev_task)
-            / f"train/model_weights/{checkpoint_name}.ckpt"
-        )
-
-        weights_path = Path(weights_dir) / f"{checkpoint_name}.ckpt"
-        shutil.move(temp_path, weights_path)
-
-        # If not weights only, add to fit kwargs
-        if weights_only:
-            logger.info(
-                f"Loading weights '{checkpoint_name}' from {checkpoint_task_id}"
-            )
-            ckpt = torch.load(weights_path, map_location="cpu")
-            config["model"].load_state_dict(ckpt["state_dict"])
-        else:
-            logger.info(
-                f"Loading checkpoint '{checkpoint_name}' from {checkpoint_task_id}"
-            )
-            trainer_fit_kwargs["ckpt_path"] = str(weights_path)
 
     logger.info("trainer_init_kwargs =")
     logger.info(print_dict(trainer_init_kwargs))
@@ -127,6 +116,7 @@ def train(
             **trainer_fit_kwargs,
         )
     except Exception as err:
+        logger.error(f"Error when running task: {str(err)}")
         if pdb:
             model = config["model"]
             train_dls = config["train_dataloaders"]
@@ -137,113 +127,100 @@ def train(
     return task.id
 
 
-from dataclasses import dataclass
-@dataclass
-class OfflineTask:
-    name: str
-    @property
-    def id(self):
-        return "offline"
-
-def setup_task(
+def get_hparams(
     hparams_path: Path,
     dev_run: bool,
-    pdb: bool,
-    offline: bool,
-) -> Tuple[Dict[str, Any], Task, Dict[str, Any]]:
-    # Import hparams
+) -> Dict[str, Any]:
     hparams = import_script_as_module(hparams_path).hparams
-
-    # Set task name and check if it's not already running
-    task_name = "-".join(Path(hparams_path).parts[-2:]).removesuffix(".py")
-    project_name = hparams["task"]["init"]["project_name"]
-
     if dev_run:
         hparams = set_hparams_debug_overrides(hparams)
-        task_name = f"DEV RUN: {task_name}"
-        project_name = "test"
-    config_path = hparams["config"].get(
-        "path",
-        str(Path(hparams_path).parent / "__init__.py"),
-    )
 
-    # Print hparams
-    logger.info("hparams =")
-    logger.info(print_dict(hparams))
-
-    logger.info(f"Using config at '{config_path}'")
-    config_fn = import_script_as_module(config_path).train_config
-    logger.info("Setting hparams on config")
-    try:
-        config = config_fn(hparams)
-    except Exception as err:
-        if pdb:
-            logger.error("Couldn't import config")
-            breakpoint()
-        raise err
-
-    task = OfflineTask(task_name)
-    if not offline:
-        # Halt if task is currently running for project_name/task_name
-        existing_task = Task.get_task(project_name=project_name, task_name=task_name)
-        if existing_task is not None and existing_task.status == "in_progress":
-            raise FileExistsError(f"Task '{project_name}/{task_name}' is in progress.")
-
-        # Unpack checkpoint kwargs
-        ckpt_params = hparams["checkpoint"]
-        checkpoint_task_id = ckpt_params.get("checkpoint_task_id")
-        checkpoint_name = ckpt_params.get("checkpoint_name", "last")
-        weights_only = bool(ckpt_params.get("weights_only", False))
-        continue_last_task = bool(
-            checkpoint_task_id and (checkpoint_name == "last") and not weights_only
-        )
-        logger.info(f"{continue_last_task = }")
-
-        # Set debug overrides
-        if dev_run and continue_last_task:
-            logger.info("(Continuing last task is disabled for debug runs)")
-            continue_last_task = False
-
-        # Start ClearML
-        # Task.add_requirements("./requirements.txt")
-        # Task.add_requirements("./requirements-dev.txt")
-        task_init_kwargs = hparams.get("task", {}).get("init", {})
-        task_init_kwargs = {
-            **task_init_kwargs,
-            "task_name": task_name,
-            "project_name": project_name,
-            "continue_last_task": continue_last_task,
-            "auto_connect_frameworks": {
-                "matplotlib": True,
-                "pytorch": False,
-                "tensorboard": True,
-            },
-        }
-        task = Task.init(**task_init_kwargs)
-        if checkpoint_task_id:
-            task.set_parent(checkpoint_task_id)
-        elif parent_task_id := hparams.get("task", {}).get("parent_task_id"):
-            task.set_parent(parent_task_id)
-
-        # Connect configurations
-        task.connect_configuration(config_path, "config")
-        task.connect(hparams, "hparams")
-
-
-    return hparams, task, config
+    return hparams
 
 
 def set_hparams_debug_overrides(hparams):
+    """"""
     # Task overrides
     hparams["task"]["init"]["project_name"] = "test"
     hparams["task"]["init"]["reuse_last_task_id"] = True
+    hparams["task"]["init"]["continue_last_task"] = False
     # Config overrides
     hparams["config"]["num_workers"] = 0
     # Trainer overrides
     hparams["trainer"]["init"]["overfit_batches"] = 1
     hparams["trainer"]["init"]["log_every_n_steps"] = 1
-    hparams["trainer"]["init"]["enable_progress_bar"] = True
     return hparams
+
+
+def compile_config(
+    hparams: Dict[str, Any],
+    config_path: Path,
+    pdb: bool,
+    field: str = "train_config",
+) -> Dict[str, Any]:
+    logger.info("hparams =")
+    logger.info(print_dict(hparams))
+
+    logger.info(f"Using config at '{config_path}'")
+
+    config_fn = getattr(import_script_as_module(config_path), field)
+    logger.info("Setting hparams on config")
+
+    try:
+        config = config_fn(hparams)
+    except Exception as err:
+        logger.error(f"Couldn't import config: {str(err)}")
+        if pdb:
+            breakpoint()
+        raise err
+
+    return config
+
+
+def load_weights(
+    hparams: Dict[str, Any],
+    config: Dict[str, Any],
+) -> Tuple[
+    Dict[str, Any],
+    Dict[str, Any],
+]:
+    ckpt_params = hparams["checkpoint"]
+    checkpoint_task_id = ckpt_params.get("checkpoint_task_id")
+    if not checkpoint_task_id:
+        return hparams, config
+
+    checkpoint_name = ckpt_params.get("checkpoint_name", "last")
+    weights_only = bool(ckpt_params.get("weights_only", False))
+    continue_last_task = bool(
+        checkpoint_task_id and (checkpoint_name == "last") and not weights_only
+    )
+    logger.info(f"{continue_last_task = }")
+
+    hparams["task"] = hparams.get("task", {})
+    hparams["task"]["init"] = hparams["task"].get("init", {})
+    if not hparams["task"]["init"].get("continue_last_task") is False:
+        hparams["task"]["init"]["continue_last_task"] = True
+
+    # TODO update to use remote checkpoint storage
+    prev_task = Task.get_task(task_id=checkpoint_task_id)
+    temp_path = (
+        ARTIFACTS_DIR
+        / f"{get_task_dir_name(prev_task)}/train/model_weights/{checkpoint_name}.ckpt"
+    )
+
+    # If not weights only, add to fit kwargs
+    if weights_only:
+        logger.info(f"Loading weights '{checkpoint_name}' from {checkpoint_task_id}")
+        ckpt = torch.load(temp_path, map_location="cpu")
+        config["model"].load_state_dict(ckpt["state_dict"])
+    else:
+        logger.info(f"Loading checkpoint '{checkpoint_name}' from {checkpoint_task_id}")
+        hparams["trainer"]["fit"] = {
+            **hparams["trainer"].get("init", {}),
+            "ckpt_path": str(temp_path),
+        }
+
+    return hparams, config
 
 
 if __name__ == "__main__":
