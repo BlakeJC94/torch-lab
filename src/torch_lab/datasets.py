1507f15f88483cc5979a0f197fa5509dbc78981c
BlakeJC94 <blakejamescook@gmail.com>
2024-04-06 08:37:30 +0200

    Docs for datasets

--- src/torch_lab/datasets.py
+++ src/torch_lab/datasets.py
@@ -1,28 +1,60 @@
+"""Base class for usage with torch_lab modules.
+
+Datasets are implemented as iterable objects which return (data, metadata) tuples on each iteration,
+where metadata is a dictionary with collate-friendly values (int, float, str, np.ndarray, or
+torch.Tensor).
+
+Labels are located in `metadata["y"]`, which are automatically looked up in the train/validation
+methods of the LabModule classes.
+
+Custom transforms can be implemented from the BaseTransform classes, wrapped into a TransformCompose
+object and passed in as an attribute
+"""
+
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Tuple
+from typing import Any, Callable, Dict, Optional, Tuple
 
 from torch import Tensor
 from torch.utils.data import Dataset
-
+from torch_lab.transforms import BaseTransform
 from torch_lab.typing import CollateData
 
 
 class BaseDataset(Dataset, ABC):
-    transform = None
-    augmentation = None
+    """Base class for implementing datasets in torch_lab."""
+
+    def __init__(
+        self,
+        transform: Optional[BaseTransform | Callable] = None,
+        augmentaion: Optional[BaseTransform | Callable] = None,
+    ):
+        """Initialise a BaseDataset.
+
+        Args:
+            transform: Transform to apply to (data, metadata) tuple output of __getitem__.
+            augmentation: Another transform to apply before `transform`.
+        """
+        self.transform = transform
+        self.augmentation = augmentaion
 
     @abstractmethod
     def __len__(self):
+        """Return length of dataset."""
         pass
 
     @abstractmethod
     def get_raw_data(self, i: int) -> Any:
+        """Return raw instance of data."""
         pass
 
     def get_raw_label(self, i: int) -> Any:
+        """Return raw instance of labels used for training/validation. Used to create
+        `metadata["y"]`.
+        """
         return None
 
     def get_additional_metadata(self, i: int) -> Dict[str, CollateData]:
+        """Add additional information to metadata."""
         return {}
 
     def get_raw(self, i: int) -> Any:
