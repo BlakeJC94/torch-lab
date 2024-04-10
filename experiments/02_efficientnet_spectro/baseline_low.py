1b25acf7ac077edaa37bea2f78efdfc77eb7b7ac
BlakeJC94 <blakejamescook@gmail.com>
2024-04-10 17:00:36 +0200

    cleanup experiements

--- experiments/02_efficientnet_spectro/baseline_low.py
+++
@@ -1,4 +0,0 @@
-from core.utils import import_script_as_module
-
-hparams = import_script_as_module("./experiments/02_efficientnet_spectro/baseline.py").hparams
-hparams["config"]["learning_rate"] = 1 * 1e-4
--- experiments/05_seizure_classification/baseline_high.py
+++
@@ -1,4 +0,0 @@
-from core.utils import import_script_as_module
-
-hparams = import_script_as_module("./experiments/08_efficientnet_spectro_resunet/baseline.py").hparams
-hparams["config"]["learning_rate"] = 3 * 1e-3
